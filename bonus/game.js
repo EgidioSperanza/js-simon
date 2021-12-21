56e15c96859192396276689e2e490a55765a2a4b
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-21 22:12:01 +0100

    Bonus

---
+++ bonus/game.js
@@ -0,0 +1,86 @@
+let userClickedPattern = [];
+let gamePattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gameStarted = false;
+let level = 0;
+const btn = document.querySelectorAll(".btn");
+const lvlTitle = document.getElementById("level-title");
+
+//Start Game
+document.addEventListener("keydown", function () {
+  if (!gameStarted) {
+    lvlTitle.textContent = "Level " + level;
+    nextSequence();
+    gameStarted = true;
+  }
+});
+
+//Click user
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    let userChosenColour = this.getAttribute("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+  });
+}
+
+//CHECK
+function checkAnswer(currentLevel) {
+  const body = document.getElementsByTagName("body")[0];
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success"); //DEBUG
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong"); //DEBUG
+    playSound("wrong");
+    body.classList.add("game-over");
+    setTimeout(function () {
+      body.classList.remove("game-over");
+    }, 200);
+    lvlTitle.textContent = "Game Over, Press Any Key to Restart";
+    startOver();
+  }
+}
+
+//Restart
+function startOver() {
+  gameStarted = false;
+  level = 0;
+  gamePattern = [];
+}
+
+//Random
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  lvlTitle.textContent = "Level " + level;
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  //DA CAPIRE MEGLIO
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+//Start Sound
+function playSound(name) {
+  let audio = new Audio("./sounds/" + name + ".mp3");
+  audio.play();
+}
+
+//Press button Animation
+function animatePress(currentColor) {
+  const currentColor = document.getElementById(currentColor);
+  currentColor.classList.add("pressed");
+  setTimeout(function () {
+    currentColor.classList.remove("pressed");
+  }, 100);
+}
--- js/main.js
+++ js/main.js
@@ -14,49 +14,54 @@ function computerNum() {
   return numbers;
 }
 
-function createBoxNum(max, arr){
-    for (let i = 0; i < max; i++) {
-        const numBox = document.createElement("div");
-        numBox.className = "game_number";
-        numBox.innerText = arr[i];
-        containerGame.append(numBox);
-      }
+function createBoxNum(max, arr) {
+  for (let i = 0; i < max; i++) {
+    const numBox = document.createElement("div");
+    numBox.className = "game_number";
+    numBox.innerText = arr[i];
+    containerGame.append(numBox);
+  }
 }
 
-function createBoxUserNum(max){
-    for (let i = 1; i <= max; i++) {
-        const numBox = document.createElement("div");
-        numBox.className = "game_number";
-        numBox.classList.add("clickable");
-        numBox.innerText = i;
-        containerGame.append(numBox);
-      }
-  
+function createBoxUserNum(max) {
+  for (let i = 1; i <= max; i++) {
+    const numBox = document.createElement("div");
+    numBox.className = "game_number";
+    numBox.classList.add("clickable");
+    numBox.innerText = i;
+    containerGame.append(numBox);
+  }
 }
 
-function compareSequence(arr, arr2, compareArr, includeArr, iCompare, iInclude){
-    for(let i=0; i<arr.length; i++){
-        if(arr[i]===arr2[i]){
-            iCompare++;
-            compareArr.push(arr[i]);
-        }else{
-            compareArr.push("x");
-        }
-        if(arr.includes(arr2[i])){
-            iInclude++;
-            includeArr.push(arr2[i])
-        }else{
-            includeArr.push("x");
-        }
+function compareSequence(
+  arr,
+  arr2,
+  compareArr,
+  includeArr,
+  iCompare,
+  iInclude
+) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr2[i]) {
+      iCompare++;
+      compareArr.push(arr[i]);
+    } else {
+      compareArr.push("x");
     }
-
+    if (arr.includes(arr2[i])) {
+      iInclude++;
+      includeArr.push(arr2[i]);
+    } else {
+      includeArr.push("x");
+    }
+  }
 }
 
 function simonGame() {
   const pcRandom = computerNum();
   const userNum = [];
 
-  createBoxNum(pcRandom.length, pcRandom)
+  createBoxNum(pcRandom.length, pcRandom);
 
   containerGame.innerHTML +=
     "<h1>Memorizza la sequenza di numeri e non trascurare il loro ordine!</h1>";
@@ -78,47 +83,55 @@ function simonGame() {
           userArr.push(i + 1);
         } else {
           userArr.push(i + 1);
-        //   console.log(userArr);//DEBUG
+          //   console.log(userArr);//DEBUG
           containerGame.innerHTML = "";
           createBoxNum(userArr.length, userArr);
           containerGame.innerHTML +=
             "<h1>Ecco i numeri che hai memorizzato</h1>";
-            let counter=0;
-            let include=0;
-            let comparePosition=[];
-            let compareInclusive=[];
-            compareSequence(pcRandom, userArr, comparePosition, compareInclusive, counter, include);
-            if (counter===pcRandom.length && counter===include){
-                containerGame.innerHTML +=
-                "<h1>Bravo hai memorizzato perfettamente la sequenza</h1>";
-            }else if(include===pcRandom.length && counter>0){
-                containerGame.innerHTML += `<h1>Hai memorizzato tutti i numeri della sequenza ma la posizione è giusta solo per ${comparePosition}</h1>`;
-            }else if(include===pcRandom.length && counter===0 ){
-                containerGame.innerHTML += `<h1>Hai memorizzato tutti numeri della sequenza ma nessunodi essi è stato posizionato correttamente. La sequenza giusta era ${pcRandom}</h1>`;
-            }else if (include===0){
-                containerGame.innerHTML+= `<h1>Come hai fatto non lo so, epppure non hai memorizzato nessun numero della sequenza che era ${pcRandom}</h1>`;
-            }else{
-                containerGame.innerHTML+= `<h1>Hai memorizzato ${include} numeri della sequenza Disponendone bene ${counter} di essi.<br />
+          let counter = 0;
+          let include = 0;
+          let comparePosition = [];
+          let compareInclusive = [];
+          compareSequence(
+            pcRandom,
+            userArr,
+            comparePosition,
+            compareInclusive,
+            counter,
+            include
+          );
+          if (counter === pcRandom.length && counter === include) {
+            containerGame.innerHTML +=
+              "<h1>Bravo hai memorizzato perfettamente la sequenza</h1>";
+          } else if (include === pcRandom.length && counter > 0) {
+            containerGame.innerHTML += `<h1>Hai memorizzato tutti i numeri della sequenza ma la posizione è giusta solo per ${comparePosition}</h1>`;
+          } else if (include === pcRandom.length && counter === 0) {
+            containerGame.innerHTML += `<h1>Hai memorizzato tutti numeri della sequenza ma nessunodi essi è stato posizionato correttamente. La sequenza giusta era ${pcRandom}</h1>`;
+          } else if (include === 0) {
+            containerGame.innerHTML += `<h1>Come hai fatto non lo so, epppure non hai memorizzato nessun numero della sequenza che era ${pcRandom}</h1>`;
+          } else {
+            containerGame.innerHTML += `<h1>Hai memorizzato ${include} numeri della sequenza Disponendone bene ${counter} di essi.<br />
                 la Sequenza corretta era ${pcRandom}</h1>`;
-            }
-            const btn = document.createElement("button")
-            containerGame.append(btn);
-            btn.className="reset";
-            btn.textContent="Gioca!";
-            btn.addEventListener("click", function(){
-                containerGame.innerHTML="";
-                simonGame();
-            })
+          }
+          const btn = document.createElement("button");
+          containerGame.append(btn);
+          btn.className = "reset";
+          btn.textContent = "Gioca!";
+          startGame(btn);
         }
       });
     }
   }, 3000);
 }
 
+function startGame(button) {
+  button.addEventListener("click", function () {
+    containerGame.innerHTML = "";
+    simonGame();
+  });
+}
+
 const containerGame = document.getElementById("game");
 const play = document.querySelector(".play");
 
-play.addEventListener("click", function() {
-  containerGame.innerHTML = "";
-  simonGame();
-});
+startGame(play);
