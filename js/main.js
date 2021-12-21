b2207f52ea044cfea7060d5f5dfacef905cbb09a
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2021-12-21 21:13:25 +0100

    scomposto in funzioni più piccole

--- js/main.js
+++ js/main.js
@@ -14,29 +14,57 @@ function computerNum() {
   return numbers;
 }
 
+function createBoxNum(max, arr){
+    for (let i = 0; i < max; i++) {
+        const numBox = document.createElement("div");
+        numBox.className = "game_number";
+        numBox.innerText = arr[i];
+        containerGame.append(numBox);
+      }
+}
+
+function createBoxUserNum(max){
+    for (let i = 1; i <= max; i++) {
+        const numBox = document.createElement("div");
+        numBox.className = "game_number";
+        numBox.classList.add("clickable");
+        numBox.innerText = i;
+        containerGame.append(numBox);
+      }
+  
+}
+
+function compareSequence(arr, arr2, compareArr, includeArr, iCompare, iInclude){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]===arr2[i]){
+            iCompare++;
+            compareArr.push(arr[i]);
+        }else{
+            compareArr.push("x");
+        }
+        if(arr.includes(arr2[i])){
+            iInclude++;
+            includeArr.push(arr2[i])
+        }else{
+            includeArr.push("x");
+        }
+    }
+
+}
+
 function simonGame() {
   const pcRandom = computerNum();
   const userNum = [];
 
-  for (let i = 0; i < pcRandom.length; i++) {
-    const numBox = document.createElement("div");
-    numBox.className = "game_number";
-    numBox.innerText = pcRandom[i];
-    containerGame.append(numBox);
-  }
+  createBoxNum(pcRandom.length, pcRandom)
+
   containerGame.innerHTML +=
     "<h1>Memorizza la sequenza di numeri e non trascurare il loro ordine!</h1>";
 
   const userArr = [];
   setTimeout(function () {
     containerGame.innerHTML = "";
-    for (let i = 1; i <= 9; i++) {
-      const numBox = document.createElement("div");
-      numBox.className = "game_number";
-      numBox.classList.add("clickable");
-      numBox.innerText = i;
-      containerGame.append(numBox);
-    }
+    createBoxUserNum(9);
     containerGame.innerHTML +=
       "<h1>Clicca sui numeri nell'ordine che sei riuscito a memorizzare poco fa.</h1>";
 
@@ -44,42 +72,22 @@ function simonGame() {
     let click = 0;
     for (let i = 0; i < limits; i++) {
       containerGame.children.item(i).addEventListener("click", function () {
-        console.log(i + 1); //DEBUG
-        console.log(click + "click");
+        // console.log(i + 1); //DEBUG
         click++;
-        console.log(click + "click");
         if (click < 5) {
           userArr.push(i + 1);
         } else {
           userArr.push(i + 1);
-          console.log(userArr);
+        //   console.log(userArr);//DEBUG
           containerGame.innerHTML = "";
-          for (let i = 0; i < userArr.length; i++) {
-            const numBox = document.createElement("div");
-            numBox.className = "game_number";
-            numBox.innerText = userArr[i];
-            containerGame.append(numBox);
-          }
+          createBoxNum(userArr.length, userArr);
           containerGame.innerHTML +=
             "<h1>Ecco i numeri che hai memorizzato</h1>";
             let counter=0;
             let include=0;
             let comparePosition=[];
             let compareInclusive=[];
-            for(let i=0; i<pcRandom.length; i++){
-                if(pcRandom[i]===userArr[i]){
-                    counter++;
-                    comparePosition.push(pcRandom[i]);
-                }else{
-                    comparePosition.push("x");
-                }
-                if(pcRandom.includes(userArr[i])){
-                    include++;
-                    compareInclusive.push(userArr[i])
-                }else{
-                    compareInclusive.push("x");
-                }
-            }
+            compareSequence(pcRandom, userArr, comparePosition, compareInclusive, counter, include);
             if (counter===pcRandom.length && counter===include){
                 containerGame.innerHTML +=
                 "<h1>Bravo hai memorizzato perfettamente la sequenza</h1>";
